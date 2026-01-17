ec286686a462528de64a773ad1c8fd24962abb96
mike cullerton <michaelc@cullerton.com>
2026-01-17 11:07:39 -0500

    Update the script template
    Added docstrings and some pylint disables

--- crc/scripts/script_template.py
+++ crc/scripts/script_template.py
@@ -1,14 +1,19 @@
-from crc.api.common import ApiError
-from crc.scripts.script import Script
+"""Template for scripts"""
+from crc.api.common import ApiError # noqa - this is our api error wrapper
+from crc.scripts.script import Script # pylint disable=unable-to-import
 
 
 class ScriptTemplate(Script):
+    """Template for creating scripts"""
 
     def get_description(self):
+        """Method that returns a description of the script."""
         return """This is my description"""
 
     def do_task_validate_only(self, task, study_id, workflow_id, *args, **kwargs):
+        """Method to validate the script."""
         return self.do_task(task, study_id, workflow_id, *args, **kwargs)
 
-    def do_task(self, task, study_id, workflow_id, *args, **kwargs):
+    def do_task(self, task, study_id, workflow_id, *args, **kwargs):  # pylint: disable=unused-argument
+        """Method to perform the task."""
         pass
