ea4f6246197ec00264c58bbf872816ca73ed3996
mike cullerton <michaelc@cullerton.com>
2026-02-11 08:01:59 -0500

    Change: Commiting changes to testing
    Commiting changes to testing
    
    Dependency: None

--- crc/scripts/local_return_to_pi.py
+++ crc/scripts/local_return_to_pi.py
@@ -22,7 +22,33 @@ class LocalReturnToPI(Script):
 
     def do_task(self, task, study_id, workflow_id, *args, **kwargs):  # pylint: disable=unused-argument
         """Method to perform the task."""
-        if 'message' in kwargs:
-            return kwargs['message']
+        if 'mode' in kwargs:
+            mode = kwargs['mode']
+            if mode == 'current':
+                sds_local_return_to_pi = task.data['data_store_get'](
+                    type='study',
+                    key='sds_local_return_to_pi')
+                sds_local_return_to_pi_message = task.data['data_store_get'](
+                    type='study',
+                    key='sds_local_return_to_pi_message')
+                return {'sds_local_return_to_pi': sds_local_return_to_pi,
+                        'sds_local_return_to_pi_message': sds_local_return_to_pi_message}
+            if mode:
+                if 'message' in kwargs:
+                    sds_local_return_to_pi = True
+                    sds_local_return_to_pi_message = kwargs['message']
+                    task.data['data_store_set'](type='study', key='sds_local_return_to_pi',
+                                                value=sds_local_return_to_pi)
+                    task.data['data_store_set'](type='study', key='sds_local_return_to_pi_message',
+                                                value=sds_local_return_to_pi_message)
+                    return {'message': 'Local Return To PI is turned on'}
+                else:
+                    raise ApiError(code='missing_parameter', message="message not provided")
+
+            else:
+                task.data['data_store_set'](type='study', key='sds_local_return_to_pi',value='')
+                task.data['data_store_set'](type='study', key='sds_local_return_to_pi_message',value='')
+                return {'message': 'Local Return To PI is turned off'}
+
         else:
-            raise ApiError(code='missing_parameter', message="message not provided")
+            raise ApiError(code='missing_parameter', message="mode not provided")
